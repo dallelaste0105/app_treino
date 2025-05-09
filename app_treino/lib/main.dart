666170fcfa2ba7e3466672198eb96ef41117defe
LeoLorde <140649576+LeoLorde@users.noreply.github.com>
2025-05-09 08:38:40 -0300

    commit 2

--- app_treino/lib/main.dart
+++ app_treino/lib/main.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp());
 }
+
+class PaginaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
+  }
+}
